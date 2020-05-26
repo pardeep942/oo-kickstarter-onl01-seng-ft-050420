3d378489e6b6f003e5ec665bff9a800c7e14738f
PARDEEP KAUR <pkaur84@aol.com>
2020-05-26 19:03:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
-class Backer
-
+lass Backer
   attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
     @backed_projects = []
-  
   end
 
   def back_project(project)
     @backed_projects << project
     project.backers << self
+
   end
-end
+
+end 
